2e84d5a95aa3762239774f1640311ea2d88a4f81
MRH <74174954+mrhhyu@users.noreply.github.com>
2021-01-14 18:30:58 +0900

    Update SimRank_star.py

--- codes/SimRank_star.py
+++ codes/SimRank_star.py
@@ -71,9 +71,9 @@ def write_to_file(result_matrix, topK, itr):
         count = 0
         for index in range (0,len(target_node_res_sorted)):
             val = target_node_res_sorted[index]
+            target_node_res[target_node_res.index(val)] = np.nan              
             if val!=0 and target_node_res.index(val)!= target_node:
                 sim_file.write(str(target_node)+','+str(target_node_res.index(val))+','+str(round(val,5))+'\n') 
-                target_node_res[target_node_res.index(val)] = np.nan  
                 count = count + 1
                 if count == topK:
                     break
